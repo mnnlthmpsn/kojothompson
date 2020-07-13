c8446cdf83c0d615b34fe03ef71d8fbc8fad6c34
mnnlthmpsn <mnnlthmpsn@outlook.com>
2020-07-13 14:40:38 +0000

    django-heroku

--- kojoThompson/settings.py
+++ kojoThompson/settings.py
@@ -124,5 +124,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dev.flava@gmail.com'
-EMAIL_HOST_PASSWORD = 'http404#k'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
+import django_heroku
+django_heroku.settings(locals())
